face72b59e856ee963d38097c5dd925d1776fb2b
wcy <1208640961@qq.com>
2017-04-22 02:19:46 +0800

    完成

--- app/src/main/java/com/wcy/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/wcy/coolweather/ChooseAreaFragment.java
@@ -21,6 +21,7 @@ import android.widget.Toast;
 import com.wcy.coolweather.db.City;
 import com.wcy.coolweather.db.County;
 import com.wcy.coolweather.db.Province;
+import com.wcy.coolweather.gson.Weather;
 import com.wcy.coolweather.util.HttpUtil;
 import com.wcy.coolweather.util.Utility;
 
@@ -105,10 +106,17 @@ public class ChooseAreaFragment extends Fragment {
                     queryCounties();
                 }else if (currentLevel==LEVEL_COUNTY){
                     String weatherId=countyList.get(position).getWeatherId();
-                    Intent intent=new Intent(getActivity(),WeatherActivity.class);
-                    intent.putExtra("weather_id",weatherId);
-                    startActivity(intent);
-                    getActivity().finish();
+                    if (getActivity() instanceof MainActivity) {
+                        Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                        intent.putExtra("weather_id", weatherId);
+                        startActivity(intent);
+                        getActivity().finish();
+                    }else if (getActivity() instanceof WeatherActivity){
+                        WeatherActivity activity= (WeatherActivity) getActivity();
+                        activity.drawerLayout.closeDrawers();
+                        activity.swipeRefresh.setRefreshing(true);
+                        activity.requestWeather(weatherId);
+                    }
                 }
             }
         });
--- app/src/main/java/com/wcy/coolweather/WeatherActivity.java
+++ app/src/main/java/com/wcy/coolweather/WeatherActivity.java
@@ -1,16 +1,20 @@
 package com.wcy.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.content.SharedPreferencesCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
@@ -20,6 +24,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.wcy.coolweather.gson.Forecast;
 import com.wcy.coolweather.gson.Weather;
+import com.wcy.coolweather.service.AutoUpdateService;
 import com.wcy.coolweather.util.HttpUtil;
 import com.wcy.coolweather.util.Utility;
 
@@ -47,8 +52,10 @@ public class WeatherActivity extends AppCompatActivity {
     private TextView carWashText;
     private TextView sportText;
     private ImageView bingPicImg;
+    public DrawerLayout drawerLayout;
+    private Button navButton;
 
-    private SwipeRefreshLayout swipeRefresh;
+    public SwipeRefreshLayout swipeRefresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,7 +67,7 @@ public class WeatherActivity extends AppCompatActivity {
         if(Build.VERSION.SDK_INT>21){
             View decorView=getWindow().getDecorView();
             decorView.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_FULLSCREEN|
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|
                             View.SYSTEM_UI_FLAG_LAYOUT_STABLE
             );
             getWindow().setStatusBarColor(0);
@@ -115,6 +122,18 @@ public class WeatherActivity extends AppCompatActivity {
         }else {
             loadBingPic();
         }
+
+        /**
+         * 切换城市按钮
+         */
+        drawerLayout= (DrawerLayout) findViewById(R.id.drawer_layout);
+        navButton= (Button) findViewById(R.id.nav_button);
+        navButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
 
     }
 
@@ -206,12 +225,25 @@ public class WeatherActivity extends AppCompatActivity {
         String sport = "运动建议: " + weather.suggestion.sport.info;
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+
+        /**
+         * 启动自动更新服务
+         */
+        if(weather!=null&&"ok".equals(weather.status)){
+            Intent intent=new Intent(this, AutoUpdateService.class);
+            startService(intent);
+            Toast.makeText(WeatherActivity.this,"已启动自动更新服务!",
+                    Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(WeatherActivity.this,"获取天气信息失败!",
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 
     /**
      * 加载必应图片
      */
-    private void loadBingPic(){
+    public void loadBingPic(){
         String requestBingPic="http://guolin.tech/api/bing_pic";
         HttpUtil.sendOkHttpRequest(requestBingPic, new Callback() {
             @Override
